69f94a8569644e8ddace7b74b20bb3c909517502
DucTriCE <trild.ce@gmail.com>
2025-01-23 19:06:04 +0700

    Initial commit

---
+++ dataset_download.py
@@ -0,0 +1,13 @@
+# import kagglehub
+
+# # Download latest version
+# path = kagglehub.dataset_download("mcocoz/aivivn-2019")
+
+# print("Path to dataset files:", path)
+
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("baotanguyen/2016-vlsp")
+
+print("Path to dataset files:", path)
